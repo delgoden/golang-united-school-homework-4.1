782a14d5d70e60f769fbfb02747736cddd7515c3
Denis Ravinski <delgoden@gmail.com>
2022-05-12 22:34:51 +0300

    implement a function, that reverses the input string

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,14 @@
 package reverse_string
 
+import "strings"
+
 func ReverseString(input string) (output string) {
 	// solution goes here
+	inputSls := strings.Split(input, "")
+	outputSls := make([]string, 0, len(inputSls))
+	for i := len(inputSls) - 1; i >= 0; i-- {
+		outputSls = append(outputSls, inputSls[i])
+	}
+	output = strings.Join(outputSls, "")
 	return output
 }
